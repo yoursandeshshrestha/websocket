efa51f0957aba743a6ac35687d8d0f40786a473a
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-09-30 11:50:58 +0530

    compeleted

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (message) => {
+    io.emit("message", message);
+  });
+});
+
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./public/index.html"));
+});
+
+server.listen(9000, () => {
+  console.log("Server Started At: 9000");
+});
